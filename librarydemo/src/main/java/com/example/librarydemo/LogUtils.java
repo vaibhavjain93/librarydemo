efae8a10fb1d400165d50f84b6c1f5eea58a5412
vaibhav <vaibhav93.jain@gmail.com>
2018-05-14 15:20:36 +0530

    new commit

--- librarydemo/src/main/java/com/example/librarydemo/LogUtils.java
+++ librarydemo/src/main/java/com/example/librarydemo/LogUtils.java
@@ -5,10 +5,10 @@ import android.util.Log;
 public class LogUtils {
 
     private static final String TAG = "Library Demo";
-    public void e(Throwable e) {
+    public static void e(Throwable e) {
         Log.e(TAG,"This is an error",e);
     }
-    public void d(String msg) {
+    public static void d(String msg) {
         Log.i(TAG,msg);
     }
 }
